c41598a7947a3635cb372e69dd9a905da57f8025
Nurul1903 <amatidanurulhida@gmail.com>
2021-06-20 20:13:32 +0700

    project idea

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        string namaDepan = "Nurul";
+        string namaBelakang = "Hidayah";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double = 3.99999999;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+        system.out.println(namaDepan + namaBelakang);
+    }
+}
