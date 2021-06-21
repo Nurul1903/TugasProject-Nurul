394ecc7f4221532d5851143b12a05475a71e1983
Nurul1903 <amatidanurulhida@gmail.com>
2021-06-21 19:44:40 +0700

    pembayaran

---
+++ src/penjualanAlatTulis/class2.java
@@ -0,0 +1,22 @@
+package penjualanAlatTulis;
+
+import toko.class1;
+
+public class class2 extends class1{
+
+    public String bayar;
+    public String namaBarang;
+    public String kembalian;
+
+
+    public void p(){
+        System.out.println("Nama Barang :" +namaBarang);
+
+    }
+    public void pembayaran(){
+        System.out.println("bayar :" +bayar);
+        System.out.println("kembalian :" +kembalian);
+    }
+
+
+}
