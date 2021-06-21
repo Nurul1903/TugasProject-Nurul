1bf6c86e12ed7ed7d3ce6a3771742a1a313f7f7f
Nurul1903 <amatidanurulhida@gmail.com>
2021-06-21 19:33:10 +0700

    TOKONURU

---
+++ src/toko/class1.java
@@ -0,0 +1,34 @@
+package toko;
+
+public class class1 {
+        public String pulpen;
+        public String penggaris;
+        public String penghapus;
+        public String pensil;
+        public String tipx;
+
+
+        public void tampil() {
+                System.out.println("Selamat Datang di Di Toko Saya");
+        }
+
+        public void plp() {
+                System.out.println("pulpen " + pulpen);
+        }
+
+        public void pgr() {
+                System.out.println("penggaris " + penggaris);
+        }
+
+        public void ph() {
+                System.out.println("penghapus " + penghapus);
+        }
+
+        public void psl() {
+                System.out.println("pensil " + pensil);
+        }
+
+        public void tp() {
+                System.out.println("tipx " + tipx);
+        }
+}
